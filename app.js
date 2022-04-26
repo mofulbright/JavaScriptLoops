0c510cfceaba14fad54e5b567ec691b04b9250fe
mofulbright <mofulbright02@gmail.com>
2022-04-26 15:27:12 -0300

    initial

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-
+for (let i = 1; i <= 100; i++){
+    if (i % 2 != 0){
+        console.log(i)
+    }
+}
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let j = 1; j <= 100; j++){
+    if (j % 3 == 0 && j % 5 == 0){
+        console.log(`${j}: FIZZBUZZ`);
+    }
+    else if (j % 3 == 0){
+        console.log(`${j}: FIZZ`)
+    }
+    else if (j % 5 == 0){
+        console.log(`${j}: BUZZ`);
+    }
+}
